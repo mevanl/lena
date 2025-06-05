fcd8fe53c62ec0e354cc0a7a6502592ba51fe4b4
evan <mevanlauderdaleyt@gmail.com>
2025-06-05 11:15:25 -0500

    Created functions load_cogs and start to automatically load in a commands

--- src/bot.py
+++ src/bot.py
@@ -10,10 +10,30 @@ dotenv.load_dotenv()
 intents = discord.Intents.default()
 intents.message_content = True 
 lena = commands.Bot(command_prefix=".", intents=intents, help_command=None)
+command_categories = {}
 
 @lena.event
 async def on_ready():
     print(f'Logged in as: {lena.user}')
 
 
-asyncio.run(lena.start(os.getenv('TOKEN')))
+async def start():
+    await load_cogs()
+    await lena.start(os.getenv('TOKEN'))
+
+async def load_cogs():
+
+    # dir is a category each holding different commands (cogs)
+    for dir in os.listdir('./cogs'):
+
+        # each category will have a list of cogs in it
+        command_categories[dir] = []
+
+        for file in os.listdir('./cogs/' + dir):
+            if file.endswith('.py'):
+                command_categories[dir].append(file)
+                await lena.load_extension(f'cogs.{dir}.{file[:-3]}') 
+
+if __name__ == "__main__":
+    asyncio.run(start())
+
