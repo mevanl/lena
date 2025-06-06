05d24cdcfd1c40bb1c95f4bb81f8977abd73fb66
evan <mevanlauderdaleyt@gmail.com>
2025-06-05 20:15:48 -0500

    Refactored coinflip to be a hybrid command

--- src/cogs/entertainment/coinflip.py
+++ src/cogs/entertainment/coinflip.py
@@ -1,5 +1,7 @@
+import discord
 import discord.ext.commands as commands
 import random
+from ..utility import config 
 
 
 coinflip_options = ['heads', 'tails']
@@ -9,16 +11,18 @@ class Coinflip(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.command()
-    async def coinflip(self, ctx, userChoice: str):
+    @commands.hybrid_command(name="coinflip", description="Flip a coin.")
+    @discord.app_commands.describe(user_choice="Pick heads or tails.")
+    @config.debug_guild_decorator()
+    async def coinflip(self, ctx, user_choice: str):
         flip = random.choice(coinflip_options)
         await ctx.send(f'The coin landed on *{flip}!*')
 
-        if userChoice[:5].lower() not in coinflip_options:
+        if user_choice[:5].lower() not in coinflip_options:
             await ctx.send('Please choose *heads* or *tails*.')
             return
 
-        if userChoice[:5].lower() == flip:
+        if user_choice[:5].lower() == flip:
             await ctx.send('***You won!***')
             return
 
